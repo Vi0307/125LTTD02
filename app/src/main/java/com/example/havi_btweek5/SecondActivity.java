e1e08e28d1612a82d62b8678456a9a7536ab8b17
Viee <trandieuhavi0307@gmail.com>
2025-09-25 15:37:34 +0700

    Thêm hiển thị Fun Fact ngẫu nhiên ở màn hình 2

--- app/src/main/java/com/example/havi_btweek5/SecondActivity.java
+++ app/src/main/java/com/example/havi_btweek5/SecondActivity.java
@@ -2,11 +2,30 @@ package com.example.havi_btweek5;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import java.util.Random;
 
 public class SecondActivity extends AppCompatActivity {
+
+    String[] funFacts = {
+            "🐧 Chim cánh cụt có thể nhảy cao gần 2 mét dưới nước.",
+            "🌙 Trên sao Hỏa có núi lửa cao gấp 3 lần Everest.",
+            "🦄 Kỳ lân từng được coi là có thật trong cổ tích.",
+            "🍫 Sô-cô-la từng được dùng như tiền tệ ở Trung Mỹ cổ đại.",
+            "🐢 Rùa có thể thở bằng mông khi ngủ đông.",
+            "🚀 Một ngày trên sao Kim dài hơn một năm của nó."
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TextView quoteText = findViewById(R.id.quoteText);
+
+        Random random = new Random();
+        int index = random.nextInt(funFacts.length);
+
+        quoteText.setText(funFacts[index]);
     }
 }
