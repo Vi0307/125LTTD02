46ea69b11346a9bccd72a1966c22f4acfc4542fe
PhanMPhuc <phanminhphuc05@gmail.com>
2025-09-25 20:20:10 +0700

    WEEK5
    fourth push - gan click screenA screenB

--- WEEK5/app/src/main/java/com/example/week5/MainActivity.java
+++ WEEK5/app/src/main/java/com/example/week5/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.week5;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,7 +12,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Kết nối với file activity_main.xml
         setContentView(R.layout.activity_main);
+
+        // Tìm 2 nút trong layout
+        Button btnA = findViewById(R.id.btn_screen_a);
+        Button btnB = findViewById(R.id.btn_screen_b);
+
+        // Gán sự kiện click cho nút A
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScreenAActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Gán sự kiện click cho nút B
+        btnB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScreenBActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
